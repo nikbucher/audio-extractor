6c752fec898b129e17d8108b765b6dab38696bb5
Niklaus Bucher <niklaus@bucher.io>
2026-03-17 15:21:20 +0000

    chore: set log level to debug to filter tao/axum trace noise

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -486,7 +486,7 @@ pub fn run() {
 	}
 
 	tauri::Builder::default()
-		.plugin(tauri_plugin_log::Builder::new().targets(log_targets).build())
+		.plugin(tauri_plugin_log::Builder::new().level(log::LevelFilter::Debug).targets(log_targets).build())
 		.plugin(tauri_plugin_dialog::init())
 		.plugin(tauri_plugin_opener::init())
 		.setup(|_app| {
